a86bcbecf17cafae1028168f29fc66e52019602a
Artur Homan <Artur.Homan@eso.com>
2025-08-22 13:27:45 +0200

    Release candidate

--- Infrastructure/production/variables.tf
+++ Infrastructure/production/variables.tf
@@ -10,6 +10,10 @@ variable "service_plan_name" {
   type = string
 }
 
+variable "service_plan_sku_name" {
+  type = string
+}
+
 variable linux_web_app_name {
   type = string
 }
@@ -22,5 +26,22 @@ variable "postgresql_flexible_server_database_name" {
   type = string
 }
 
+variable "virtual_network_name" {
+  type = string
+}
 
+variable "frontend_subnet_name" {
+  type = string
+}
 
+variable "backend_subnet_name" {
+  type = string
+}
+
+variable "private_dns_zone_name" {
+  type = string
+}
+
+variable "private_dns_zone_virtual_network_link_name" {
+  type = string
+}
