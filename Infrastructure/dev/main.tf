34c0ecd2991da34425a6cc46f49c05f99a0d21f4
Artur Homan <Artur.Homan@eso.com>
2025-08-22 13:07:38 +0200

    MAJOR update

--- Infrastructure/dev/main.tf
+++ Infrastructure/dev/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_service_plan" "asp" {
   resource_group_name = azurerm_resource_group.rga.name
   location            = azurerm_resource_group.rga.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = var.service_plan_sku_name
   depends_on = [
     azurerm_resource_group.rga
   ]
@@ -23,14 +23,14 @@ resource "azurerm_linux_web_app" "app" {
 
   site_config {
     always_on = true
-    
+
     # application_stack {
     #   docker_image_name   = "nginx:latest"
     # }
   }
 
   app_settings = {
-    "DATABASE_URL" = "postgresql://${azurerm_postgresql_flexible_server.server.administrator_login}:${azurerm_postgresql_flexible_server.server.administrator_password}@${azurerm_postgresql_flexible_server.server.fqdn}:5432/${azurerm_postgresql_flexible_server_database.db.name}?sslmode=require"
+    "DATABASE_URL"                        = "postgresql://${azurerm_postgresql_flexible_server.server.administrator_login}:${azurerm_postgresql_flexible_server.server.administrator_password}@${azurerm_postgresql_flexible_server.server.fqdn}:5432/${azurerm_postgresql_flexible_server_database.db.name}?sslmode=require"
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
 
@@ -51,10 +51,10 @@ resource "azurerm_postgresql_flexible_server" "server" {
   administrator_password = "H@Sh1CoR3!"
   storage_mb             = 32768
   sku_name               = "GP_Standard_D4s_v3"
-  
+
   # Configure for private access
-  delegated_subnet_id    = azurerm_subnet.backend.id
-  private_dns_zone_id    = azurerm_private_dns_zone.postgres.id
+  delegated_subnet_id           = azurerm_subnet.backend.id
+  private_dns_zone_id           = azurerm_private_dns_zone.postgres.id
   public_network_access_enabled = false
 
   depends_on = [
@@ -72,9 +72,9 @@ resource "azurerm_postgresql_flexible_server_database" "db" {
   charset   = "UTF8"
 
   # prevent the possibility of accidental data loss
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 
   depends_on = [
     azurerm_postgresql_flexible_server.server
@@ -84,14 +84,14 @@ resource "azurerm_postgresql_flexible_server_database" "db" {
 
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "example-network"
+  name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rga.location
   resource_group_name = azurerm_resource_group.rga.name
 }
 
 resource "azurerm_subnet" "frontend" {
-  name                 = "frontend"
+  name                 = var.frontend_subnet_name
   resource_group_name  = azurerm_resource_group.rga.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -111,7 +111,7 @@ resource "azurerm_subnet" "frontend" {
 }
 
 resource "azurerm_subnet" "backend" {
-  name                 = "backend"
+  name                 = var.backend_subnet_name
   resource_group_name  = azurerm_resource_group.rga.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -127,12 +127,12 @@ resource "azurerm_subnet" "backend" {
   depends_on = [
     azurerm_virtual_network.vnet
   ]
-  
+
 }
 
 # Private DNS Zone for PostgreSQL
 resource "azurerm_private_dns_zone" "postgres" {
-  name                = "privatelink.postgres.database.azure.com"
+  name                = var.private_dns_zone_name
   resource_group_name = azurerm_resource_group.rga.name
 
   depends_on = [
@@ -142,7 +142,7 @@ resource "azurerm_private_dns_zone" "postgres" {
 
 # Link Private DNS Zone to VNet
 resource "azurerm_private_dns_zone_virtual_network_link" "postgres" {
-  name                  = "postgres-dns-link"
+  name                  = var.private_dns_zone_virtual_network_link_name
   resource_group_name   = azurerm_resource_group.rga.name
   private_dns_zone_name = azurerm_private_dns_zone.postgres.name
   virtual_network_id    = azurerm_virtual_network.vnet.id
@@ -163,10 +163,3 @@ resource "azurerm_app_service_virtual_network_swift_connection" "app_vnet_integr
     azurerm_subnet.frontend
   ]
 }
-
-
-
-
-
-
-
